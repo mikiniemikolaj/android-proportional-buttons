c8d797995dcf970014f9c35cc098fca7bd0b252e
mikiniemikolaj <michalpiorkowski@op.pl>
2026-09-24 13:23:34 +0200

    Add 3 full width buttons in activity_main.xml and MainActivity.java

--- app/src/main/java/com/example/cwiczenie51mp_18/MainActivity.java
+++ app/src/main/java/com/example/cwiczenie51mp_18/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.cwiczenie51mp_18;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button1;
+    private Button button2;
+    private Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,13 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+
+        button1.setOnClickListener(v -> Toast.makeText(this, "Kliknięto Przycisk 1", Toast.LENGTH_SHORT).show());
+        button2.setOnClickListener(v -> Toast.makeText(this, "Kliknięto Przycisk 2", Toast.LENGTH_SHORT).show());
+        button3.setOnClickListener(v -> Toast.makeText(this, "Kliknięto Przycisk 3", Toast.LENGTH_SHORT).show());
     }
 }
